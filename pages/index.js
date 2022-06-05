fdffd07ee14f44a4d6ead566f6167ae7a94ccd09
harisw <hari14@mhs.if.its.ac.id>
2022-06-06 08:01:29 +0900

    Add popular manga with better UI

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,11 @@ import Image from 'next/image'
 
 import Navbar from '../components/Navbar';
 const coverPlaceholder = "https://static.mangajar.com/posters/4529/NhKZN9lmEkCFDSpNwZVyP5x8jKrthgXRRmVff7Yz_mini.jpg";
+const API_URL = "http://mangareo.azurewebsites.net";
 
-const Home = ({ data }) => {
+const Home = ({ updateds, populars }) => {
   // console.log(data);
-  const updatedManga = data.map(m => {
-    console.log(m.title);
+  const updatedManga = updateds.map(m => {
     return (
       <div class="flex flex-col items-center justify-center w-40">  
         <div class="w-full h-32 bg-gray-300 bg-center bg-cover rounded-lg shadow-md" style={{backgroundImage: `url(${coverPlaceholder})`}}></div>
@@ -18,37 +18,38 @@ const Home = ({ data }) => {
         </div>
     </div>
     );
-  })
+  });
+
+  let popularManga = [];
+  for(let i=0; i< 10; i++){
+    popularManga.push(<h1 className='text-base font-bold truncate'>{i+1}. {populars[i].title}</h1>);
+  }
+
   return (
     <div className="flex min-h-screen flex-col items-center justify-center bg-yellow-300">
       <Head>
-        <title>Create Next App</title>
+        <title>Mangareo - Your one stop place to explore Manga</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Navbar />
-      <main className="flex w-full flex-1 flex-col items-center content-start px-16 text-center">
+      <main className="flex w-full flex-1 flex-col items-center content-start px-8 text-center">
         <h1 className="text-5xl font-bold">
-          <a className="text-black" href="https://nextjs.org">
+          <a className="text-black" href="/">
             Mangareo!
           </a>
         </h1>
 
-        <div className="mt-4 flex w-full items-center justify-around sm:w-full">
-          <div className='bg-cyan-400 w-3/4 h-auto px-2 py-2'>
+        <div className="mt-4 flex w-full justify-around sm:w-full space-x-6">
+          <div className='bg-cyan-400 w-3/4 h-auto px-4 py-10 space-y-1 rounded shadow-xl'>
             <h1 className='text-3xl font-bold text-left'>Last manga updates</h1>
-            <div className='grid grid-cols-5 auto-cols-max gap-2'>
+            <div className='grid grid-cols-5 auto-cols-max gap-6'>
               {updatedManga}
             </div>
           </div>
-          <div className='bg-cyan-400 w-1/4'>
-            <h1 className='text-3xl font-bold'>Top rated manga</h1>
-            <h1 className='text-xl font-bold'>1. </h1>
-            <h1 className='text-xl font-bold'>1. </h1>
-            <h1 className='text-xl font-bold'>1. </h1>
-            <h1 className='text-xl font-bold'>1. </h1>
-            <h1 className='text-xl font-bold'>1. </h1>
-            <h1 className='text-xl font-bold'>1. </h1>
-            <h1 className='text-xl font-bold'>1. </h1>
+
+          <div className='bg-cyan-400 w-1/4 text-left px-4 space-y-1 h-fit py-10 rounded shadow-xl'>
+            <h1 className='text-3xl font-bold'>Popular manga</h1>
+            {popularManga}
           </div>
         </div>
       </main>
@@ -69,13 +70,18 @@ const Home = ({ data }) => {
 }
 
 export const getServerSideProps = async () => {
-  // const resp = await fetch("http://localhost:8080/api/manga/most-viewed");
-  const URL = "http://localhost:8080/api/manga/last-updated";
-  //console.log(JSON.parse(JSON.stringify(resp)));
-  const res = await fetch(URL);
-  const data = await res.json()
-
-  // Pass data to the page via props
-  return { props: { data } }
+  const [updatedRes, popularRes] = await Promise.all([
+    fetch(`${API_URL}/api/manga/last-updated`), 
+    fetch(`${API_URL}/api/manga/most-viewed`)
+  ]);
+  const [updateds, populars] = await Promise.all([
+    updatedRes.json(), 
+    popularRes.json()
+  ]);
+  return { 
+    props: {
+       updateds, populars
+    }
+  }
 }
 export default Home;
